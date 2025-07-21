f3ce7f2089a9a3985b207ec163101ca9753af328
ck989 <chayakumar33@gmail.com>
2025-07-20 20:54:38 -0700

    conanfile.py changes to comply with conan 2.x version

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,6 @@
 import os
-from pathlib import pathlib
-from conans import ConanFile
+from conan.tools.files import copy
+from conan import ConanFile
 
 class Package(ConanFile):
     name = "gcc-arm-none-eabi"
@@ -9,14 +9,13 @@ class Package(ConanFile):
     def build(self):
         url = "https://github.com/ck989/gcc-arm-none-eabi.git"
         self.run(f"git clone --depth=1 {url} gcc")
-
         return
     
     def package(self):
-        self.copy("*", src="gcc", dst="")
+        copy(self, pattern="*", dst=self.package_folder, src=os.path.join(self.source_folder, "gcc"))
         return
     
     def package_info(self):
-        self.env_info.path.append(os.path.join(self.package_folder, "bin"))
+        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
         return
 
